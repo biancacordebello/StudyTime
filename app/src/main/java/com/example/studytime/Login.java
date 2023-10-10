955d30c799a6bcb56f1a7c5f6a9641fc020de9d9
Bianca Cordebello <biancagcordebello@gmail.com>
2023-10-10 00:43:42 -0300

    Added register Page

--- app/src/main/java/com/example/studytime/Login.java
+++ app/src/main/java/com/example/studytime/Login.java
@@ -2,9 +2,14 @@ package com.example.studytime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private TextView register_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,20 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+        AbrirTelaCadastro();
+
+        register_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,Cadastro.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void AbrirTelaCadastro(){
+        register_text = findViewById(R.id.register_text);
+
     }
 }
